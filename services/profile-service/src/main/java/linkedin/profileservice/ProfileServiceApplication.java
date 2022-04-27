4f83077e653ded3fcfd8b22eb8414fa60f1cc115
masa.bursac <bursac.ra133.2018@uns.ac.rs>
2022-04-27 18:19:59 +0200

    database connected
    deleted hardcoded data

--- services/profile-service/src/main/java/linkedin/profileservice/ProfileServiceApplication.java
+++ services/profile-service/src/main/java/linkedin/profileservice/ProfileServiceApplication.java
@@ -15,30 +15,8 @@ import linkedin.profileservice.service.Implementation.SequenceGeneratorService;
 
 @SpringBootApplication
 public class ProfileServiceApplication {
-
-	static ProfileRepository pr;
-	static AuthRepository ar;
-	static SequenceGeneratorService sequenceGeneratorService;
-	@Autowired
-	public ProfileServiceApplication(ProfileRepository pr, AuthRepository ar, SequenceGeneratorService sg) {
-		this.pr = pr;
-		this.ar = ar;
-		this.sequenceGeneratorService = sg;
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(ProfileServiceApplication.class, args);
-		Profile profile = new Profile();
-		profile.setBiography("happy");
-		profile.setIsPrivate(true);
-		UserInfo us= new UserInfo();
-		us.setUsername("milan");
-		us.setPassword("1234");
-        us.setId((int) sequenceGeneratorService.generateSequence(UserInfo.SEQUENCE_NAME));
-
-		ar.save(us);
-        profile.setId((int) sequenceGeneratorService.generateSequence(Profile.SEQUENCE_NAME));
-		System.out.println(profile);
-		pr.save(profile);
 		
 	}
 
